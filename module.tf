c9c05ad1e832f4315a7c2474629315b1329eb01e
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 12:34:36 +0530

    Create module.tf

---
+++ module.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source  = "spacelift.io/citagi63/vpc/default"
+  version = "0.1.0"
+
+  # Required inputs 
+  app_port                = # string
+  aws_default_region      = # string
+  environment             = # string
+  main_pvt_route_table_id = # string
+  private_subnet_tag_name = # string
+  public_subnet_tag_name  = # string
+  vpc_tag_name            = # string
+
+  # Optional inputs 
+  # availability_zones            = list(string)
+  # number_of_private_subnets     = number
+  # number_of_private_subnets_db  = number
+  # number_of_public_subnets      = number
+  # private_subnet_cidr_blocks    = list(string)
+  # private_subnet_cidr_blocks_db = list(string)
+  # public_subnet_cidr_blocks     = list(string)
+  # vpc_cidr_block                = string
+}
