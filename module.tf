ba92cf2666258b8e19a0cfe6c719dfcf6d9ab41c
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 12:47:24 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -19,5 +19,5 @@ module "vpc" {
    private_subnet_cidr_blocks    = ["10.0.1.0/24" , "10.0.2.0/24"]
    private_subnet_cidr_blocks_db =  ["10.0.3.0/24" , "10.0.4.0/24"]
    public_subnet_cidr_blocks     =  ["10.0.5.0/24" , "10.0.6.0/24"]
-  vpc_cidr_block                = ["10.0.0.0/16"]
+  vpc_cidr_block                = "10.0.0.0/16"
 }
