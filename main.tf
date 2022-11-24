ecb1264a97345684bd273c4a01f29a7b08d8d766
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 15:09:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 module "vpc" {
   source = "./vpc"
   app_port = var.app_port
+  vpc_tag_name= var.vpc_tag_name
+  main_pvt_route_table_id = var.main_pvt_route_table_id
+  environment = var.environment
+  public_subnet_tag_name = var.public_subnet_tag_name
+  aws_default_region = var.aws_default_region
+  vpc_tag_name = var.vpc_tag_name
+  private_subnet_tag_name = var.private_subnet_tag_name
 }
