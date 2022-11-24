5770f3ff16d2e51c7b99a1cf8eef5a4c437783ce
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 17:43:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,4 +25,6 @@ module "ecs_fargate" {
   app_image = "nginx"
   vpc_id = module.vpc.vpc_tag_name.id
   region = module.vpc.aws_default_region
+  environment = module.vpc.environment
+   private_subnet_ids = module.vpc.private_subnet_tag_name.id
   }
