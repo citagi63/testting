fa3117bdf103b7e28f6c7e574459c7b59e27bc86
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 17:46:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,12 +19,3 @@ module "ecs-cluster" {
   cluster_name                  = "conductor"
   cluster_tag_name              = "conductor"
 }
-module "ecs_fargate" {
-  source = "./ecs-fargate"
-  alb_name  = "conductor"
-  app_image = "nginx"
-  vpc_id = module.vpc.vpc_tag_name.id
-  region = module.vpc.aws_default_region
-  environment = module.vpc.environment
-   private_subnet_ids = module.vpc.private_subnet_tag_name.id
-  }
