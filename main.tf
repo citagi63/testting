57e8171fa1ead99f7625ec64cf0b1c4f87a94c32
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 15:26:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
 module "vpc" {
-  source = "./vpc"
-  app_port = 80
-  vpc_tag_name= var.vpc_tag_name
-  main_pvt_route_table_id = var.main_pvt_route_table_id
-  environment = var.environment
-  public_subnet_tag_name = var.public_subnet_tag_name
-  aws_default_region = var.aws_default_region
-  #vpc_tag_name = var.vpc_tag_name
-  private_subnet_tag_name = var.private_subnet_tag_name
+  source  = "spacelift.io/citagi63/vpc/aws"
+  version = "0.1.0"
+
+  # Required inputs 
+  app_port                = var.app_port
+  aws_default_region      = # string
+  environment             = # string
+  main_pvt_route_table_id = # string
+  private_subnet_tag_name = # string
+  public_subnet_tag_name  = # string
+  vpc_tag_name            = # string
+
+  # Optional inputs 
+  # availability_zones            = list(string)
+  # number_of_private_subnets     = number
+  # number_of_private_subnets_db  = number
+  # number_of_public_subnets      = number
+  # private_subnet_cidr_blocks    = list(string)
+  # private_subnet_cidr_blocks_db = list(string)
+  # public_subnet_cidr_blocks     = list(string)
+  # vpc_cidr_block                = string
 }
