6c9adf921b72377739d0992e5c93bec12bd51547
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 14:52:55 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -3,7 +3,7 @@ module "vpc" {
   version = "0.1.0"
 
   # Required inputs 
-  app_port                =  80
+  app_port                =  var.app_port
   aws_default_region      =  "us-east-2"
   environment             =  "dev"
   main_pvt_route_table_id =  "main"
@@ -20,29 +20,4 @@ module "vpc" {
    private_subnet_cidr_blocks_db =  ["10.0.3.0/24" , "10.0.4.0/24"]
    public_subnet_cidr_blocks     =  ["10.0.5.0/24" , "10.0.6.0/24"]
   vpc_cidr_block                = "10.0.0.0/16"
-}
- module "ecs-cluster" {
-  source  = "spacelift.io/citagi63/ecs-cluster/default"
-  version = "0.1.0"
-
-  # Required inputs 
-  cluster_name     = var.cluster_name # string
-  cluster_tag_name = "conductor-server"# string
-}
-module "ecs-fargate" {
-  source  = "spacelift.io/citagi63/ecs-fargate/default"
-  version = "0.1.0"
-
-  # Required inputs 
-  alb_name           = var.alb_name # string
-  app_image          = var.app_image # string
-  environment        = var.environment# string
-  private_subnet_ids = var.private_subnet_tag_name.id # list(string)
-  region             = "us-east-2"# string
-  #cluster_name       = var.cluster_name
-  vpc_id             = var.vpc_tag_name.id# string
-
-  # Optional inputs 
-  #container_port = 80
-  #ecr_repo       = "conductor"
 }
