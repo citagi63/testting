e3a9d02dd859e49c10091435f6ff4e0cbd975ec4
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 18:00:38 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,14 +6,6 @@ output vpc_id {
   value = module.vpc.vpc_id
 }
 
-output private_subnet_ids {
-  value = module.vpc.private_subnet_ids
-}
-
-output ecs_tasks_security_group_id {
-  value = aws_security_group.ecs_tasks.id
-}
-
 output main_pvt_route_table_id {
   value = module.vpc.main_pvt_route_table_id
 }
