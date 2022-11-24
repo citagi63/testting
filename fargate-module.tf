f5755c5dc92fd0d628b8c278db13324318b84907
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 18:05:58 +0530

    Update fargate-module.tf

--- fargate-module.tf
+++ fargate-module.tf
@@ -5,5 +5,5 @@ module "ecs-fargate" {
   vpc_id = module.vpc.vpc_tag_name.id
   region = var.aws_default_region
   environment = var.environment
-   private_subnet_ids = module.vpc.private_subnet_tag_name.id
+   private_subnet_ids = module.vpc.private_subnet_id
   }
