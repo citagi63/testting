c80afaefdf7f5bb9b475fd0a9b41350d77498103
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-24 15:09:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ module "vpc" {
   environment = var.environment
   public_subnet_tag_name = var.public_subnet_tag_name
   aws_default_region = var.aws_default_region
-  vpc_tag_name = var.vpc_tag_name
+  #vpc_tag_name = var.vpc_tag_name
   private_subnet_tag_name = var.private_subnet_tag_name
 }
