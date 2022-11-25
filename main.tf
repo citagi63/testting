a9b5961314cb92869286ac08b4d41c60e320dafb
root <root@ip-172-31-47-84.us-east-2.compute.internal>
2022-11-25 09:55:33 +0000

    asdhref

---
+++ main.tf
@@ -0,0 +1,48 @@
+#####################
+######## VPC ########
+#####################
+
+module "vpc_ecs" {
+  source                  = "../../blueprints/vpc-ecs"
+  app_port                = ""
+  availability_zones      = []
+  aws_default_region      = ""
+  environment             = ""
+  main_pvt_route_table_id = ""
+  platform_name           = ""
+  private_subnet_tag_name = ""
+  public_subnet_tag_name  = ""
+  vpc_tag_name            = ""
+}
+
+#####################
+#### ECS Cluster ####
+#####################
+
+module "ecs_cluster" {
+  source           = "../../modules/ecs-cluster"
+  cluster_tag_name = ""
+  name             = ""
+}
+
+#########################################
+#### ECS task definition and service ####
+#########################################
+
+module "ecs_fargate" {
+  source                           = "../../modules/ecs-fargate"
+  app_count                        = ""
+  app_image                        = ""
+  app_port                         = 0
+  aws_default_region               = ""
+  aws_security_group_ecs_tasks_id  = ""
+  cluster_id                       = ""
+  enable_cross_zone_load_balancing = ""
+  environment                      = ""
+  fargate_cpu                      = 0
+  fargate_memory                   = 0
+  name                             = ""
+  private_subnet_ids               = []
+  vpc_id                           = ""
+}
+
