03ad17cde86525508038e7b8fb0a63c1f134bc86
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-25 15:27:39 +0530

    Update main.tf

--- blueprints/vpc/main.tf
+++ blueprints/vpc/main.tf
@@ -14,3 +14,66 @@ module "vpc" {
   public_subnet_tag_name        = "${var.platform_name}-public-subnet"
   number_of_public_subnets      = 2
 }
+
+  resource "aws_security_group" "lb" {
+  name        = "${var.security_group_lb_name}-${var.environment}"
+  description = var.security_group_lb_description
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.security_group_ecs_tasks_name}-${var.environment}"
+  description = var.security_group_ecs_tasks_description
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 443
+    to_port         = 443
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [
+      aws_vpc_endpoint.s3.prefix_list_id
+    ]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
