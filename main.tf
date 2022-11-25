95845ba745f741f039c906a89c02f3bdb579b1bf
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-25 15:19:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,80 +1,47 @@
-module "vpc" {
-  source                        = "./vpc"
-  vpc_tag_name                  = "${var.platform_name}-vpc"
-  number_of_private_subnets     = 2
-  number_of_private_subnets_db  = 2
-  private_subnet_tag_name       = "${var.platform_name}-private-subnet"
-  #route_table_tag_name          = "${var.platform_name}-rt"
-  environment                   = var.environment
-  #security_group_lb_name        = "${var.platform_name}-alb-sg"
-  #security_group_ecs_tasks_name = "${var.platform_name}-ecs-tasks-sg"
-  app_port                      = var.app_port
-  main_pvt_route_table_id       = module.vpc.main_pvt_route_table_id
-  availability_zones            = var.availability_zones
-  aws_default_region            = var.aws_default_region
-  public_subnet_tag_name        = "${var.platform_name}-public-subnet"
-  number_of_public_subnets      = 2
-}
- 
-  resource "aws_security_group" "lb" {
-  name        = "${var.security_group_lb_name}-${var.environment}"
-  description = var.security_group_lb_description
-  vpc_id      = module.vpc.vpc_id
+#####################
+######## VPC ########
+#####################
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 8080
-    to_port     = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "vpc_ecs" {
+  source                  = "../../blueprints/vpc-ecs"
+  app_port                = ""
+  availability_zones      = []
+  aws_default_region      = ""
+  environment             = ""
+  main_pvt_route_table_id = ""
+  platform_name           = ""
+  private_subnet_tag_name = ""
+  public_subnet_tag_name  = ""
+  vpc_tag_name            = ""
 }
 
+#####################
+#### ECS Cluster ####
+#####################
 
-resource "aws_security_group" "ecs_tasks" {
-  name        = "${var.security_group_ecs_tasks_name}-${var.environment}"
-  description = var.security_group_ecs_tasks_description
-  vpc_id      = module.vpc.vpc_id
+module "ecs_cluster" {
+  source           = "../../modules/ecs-cluster"
+  cluster_tag_name = ""
+  name             = ""
+}
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = var.app_port
-    to_port     = var.app_port
-    cidr_blocks = [var.vpc_cidr_block]
-  }
+#########################################
+#### ECS task definition and service ####
+#########################################
 
-  ingress {
-    protocol        = "tcp"
-    from_port       = 443
-    to_port         = 443
-    cidr_blocks = [var.vpc_cidr_block]
-  }
-
-  egress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-  }
-  egress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    prefix_list_ids = [
-      aws_vpc_endpoint.s3.prefix_list_id
-    ]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "ecs_fargate" {
+  source                           = "../../modules/ecs-fargate"
+  app_count                        = ""
+  app_image                        = ""
+  app_port                         = 0
+  aws_default_region               = ""
+  aws_security_group_ecs_tasks_id  = ""
+  cluster_id                       = ""
+  enable_cross_zone_load_balancing = ""
+  environment                      = ""
+  fargate_cpu                      = 0
+  fargate_memory                   = 0
+  name                             = ""
+  private_subnet_ids               = []
+  vpc_id                           = ""
 }
