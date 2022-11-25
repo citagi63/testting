5a3d4c1ac2579fcaa91eb7de157561fd6c3bc6c1
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-25 15:00:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,6 +78,6 @@ resource "aws_security_group" "ecs_tasks" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-module "openserach" {
-  source = "./openserch"
+module "opensearch" {
+  source = "./opensearch"
 }
