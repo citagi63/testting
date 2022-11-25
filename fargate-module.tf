f6609d93bf79d2ccaaf5ced52014c3eb82e5cb2a
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-25 09:55:59 +0530

    Update fargate-module.tf

--- fargate-module.tf
+++ fargate-module.tf
@@ -1,9 +1,9 @@
-module "ecs-fargate" {
-  source = "./ecs-fargate"
-  alb_name  = "conductor"
-  app_image = "nginx"
-  vpc_id = module.vpc.vpc_tag_name.id
-  region = var.aws_default_region
-  environment = var.environment
-   private_subnet_ids = module.vpc.private_subnet_ids
-  }
+#module "ecs-fargate" {
+ # source = "./ecs-fargate"
+ # alb_name  = "conductor"
+ # app_image = "nginx"
+ # vpc_id = module.vpc.vpc_tag_name.id
+ # region = var.aws_default_region
+ # environment = var.environment
+  # private_subnet_ids = module.vpc.private_subnet_ids
+  #}
