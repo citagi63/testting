109d42e60f81b357d9de690074597db83027b645
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-25 15:58:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,18 +31,18 @@ module "ecs-cluster" {
 
 module "ecs-fargate" {
   source                           = "./module/ecs-fargate"
-  app_count                        = ""
+  #app_count                        = ""
   app_image                        = ""
   app_port                         = 0
-  aws_default_region               = ""
-  aws_security_group_ecs_tasks_id  = ""
+  #aws_default_region               = ""
+  #aws_security_group_ecs_tasks_id  = ""
   cluster_id                       = ""
-  enable_cross_zone_load_balancing = ""
+  #enable_cross_zone_load_balancing = ""
   environment                      = ""
-  fargate_cpu                      = 0
-  fargate_memory                   = 0
-  name                             = ""
-  private_subnet_ids               = []
+  #fargate_cpu                      = 0
+  #fargate_memory                   = 0
+  #name                             = ""
+  #private_subnet_ids               = []
   vpc_id                           = ""
 }
 
