f0ec61cbd2f2993b6311a9226b5f303dba121221
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-25 15:24:39 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "aws_default_region" {}
+
+variable "org_name" {}
+variable "platform_name" {}
+
+variable "security_account_id" {}
+
+variable "environment" {}
+
+variable "account_id" {}
+
+variable "deployment_service_account_user_arn" {}
