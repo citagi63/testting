9c9963c99de2cbfd3a10f26e0889648ea2010e51
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-28 11:24:01 +0530

    Update output.tf

--- module/ecs-fargate/output.tf
+++ module/ecs-fargate/output.tf
@@ -15,7 +15,7 @@ output aLb-name {
   value = aws_lb.alb.name
 }
 output security-group-id {
-  value = aws_security_group.allow_alb.id
+  value = aws_security_group.lb.id
 }
 output image {
   value = var.app_image
