dbf49c66493d2c5609d7167b043f6cec4007660c
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-11-29 10:20:25 +0530

    Update vpc-endpoints.tf

--- blueprints/vpc/vpc-endpoints.tf
+++ blueprints/vpc/vpc-endpoints.tf
@@ -54,7 +54,7 @@ resource "aws_vpc_endpoint" "cloudwatch" {
 # S3
     resource "aws_vpc_endpoint" "s3" {
   vpc_id       = module.vpc.vpc_id
-  service_name = "com.amazonaws.${var.aws_default_region}.s3"
+  service_name = "com.amazonaws.us-east-1.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids = ["${module.vpc.main_pvt_route_table_id}"]
 
