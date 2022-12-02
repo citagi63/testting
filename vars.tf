7c9f762f666f8a4023be725c5419f9b5374a1065
citagi63 <103110800+citagi63@users.noreply.github.com>
2022-12-02 19:08:24 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,7 +2,9 @@ variable "aws_default_region" {}
 
 variable "org_name" {}
 variable "platform_name" {}
-variable "availability_zones" {}
+variable "availability_zones" {
+  type= list(string)
+}
 #variable "security_group_id" {}
 
 variable "environment" {}
